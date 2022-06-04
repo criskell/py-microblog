7f2cd379d5b6891b1fc2c44385a6902a4a605721
Cristiano <cris.moraes.ds@gmail.com>
2022-06-04 18:24:35 -0300

    Adicionar a estrutura de formulários e configurações

---
+++ app/forms.py
@@ -0,0 +1,10 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired
+
+
+class LoginForm(FlaskForm):
+    username = StringField('Nome de usuário', validators=[DataRequired()])
+    password = PasswordField('Senha', validators=[DataRequired()])
+    remember_me = BooleanField('Lembrar-me')
+    submit = SubmitField('Entrar')
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,8 @@
-# parei no capitulo 2
+from flask import render_template, flash, redirect, url_for
 
-from flask import render_template
 from app import app
+from app.forms import LoginForm
+
 
 @app.route('/')
 def index():
@@ -17,3 +18,14 @@ def index():
         }
     ]
     return render_template('index.html', title='Página inicial', user=user, posts=posts)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        flash("Os dados foram enviados com sucesso. Seu usuário é {} e sua senha é {}"
+            .format(form.username.data, form.password.data))
+        redirect(url_for("index"))
+
+    return render_template('login.html', title='Entrar', form=form)
