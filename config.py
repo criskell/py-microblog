6600ab358ee19592c84a5239eca456dea365543d
Cristiano <cris.moraes.ds@gmail.com>
2022-06-04 21:38:24 -0300

    Adicionar banco de dados

--- config.py
+++ config.py
@@ -1,4 +1,9 @@
 import os
 
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY', 'COLOQUE-UMA-CHAVE-SUPERSECRETA-AQUI')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
