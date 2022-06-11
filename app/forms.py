bd57c6ba783d711a503027e438e86514bdb04981
Cristiano <cris.moraes.ds@gmail.com>
2022-06-11 17:29:44 -0300

    Adicionar a classe de formulário de registro

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,9 @@
+from tkinter.messagebox import NO
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Email, EqualTo, ValidationError
+
+from app.models import User
 
 
 class LoginForm(FlaskForm):
@@ -8,3 +11,22 @@ class LoginForm(FlaskForm):
     password = PasswordField('Senha', validators=[DataRequired()])
     remember_me = BooleanField('Lembrar-me')
     submit = SubmitField('Entrar')
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Nome de usuário', validators=[DataRequired()])
+    email = StringField('E-mail', validators=[DataRequired(), Email()])
+    password = PasswordField('Senha', validators=[DataRequired()])
+    password_confirmation = PasswordField('Confirmação de senha', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Criar nova conta')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+
+        if user is not None:
+            raise ValidationError("Um usuário com este nome de usuário já existe.")
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+
+        if user is not None:
+            raise ValidationError("Um usuário com este e-mail já existe.")
