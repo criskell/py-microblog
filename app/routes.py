0cdf668c990360aaada8ec8c5415b877068fdb51
Cristiano <cris.moraes.ds@gmail.com>
2022-06-11 17:29:56 -0300

    Adicionar a rota para registrar-se

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,8 @@
 from flask import render_template, flash, redirect, url_for, request
 from flask_login import current_user, login_user, logout_user, login_required
 
-from app import app
-from app.forms import LoginForm
+from app import app, db
+from app.forms import LoginForm, RegistrationForm
 from app.models import User
 from app.util import is_absolute_url
 
@@ -58,3 +58,22 @@ def logout():
     logout_user()
     return redirect(url_for('index'))
 
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+
+    form = RegistrationForm()
+    
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+
+        user.set_password(form.password.data)
+
+        db.session.add(user)
+        db.session.commit()
+
+        flash('Parabéns. Você criou uma conta')
+        return redirect(url_for('login'))
+    
+    return render_template('register.html', title='Registrar-se', form=form)
