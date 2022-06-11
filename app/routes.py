cb22bed8cc1224a9ba479457a833130359f14159
Cristiano <cris.moraes.ds@gmail.com>
2022-06-11 16:16:10 -0300

    Adicionar a lógica de login na view function

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,9 @@
 from flask import render_template, flash, redirect, url_for
+from flask_login import current_user, login_user, logout_user
 
 from app import app
 from app.forms import LoginForm
+from app.models import User
 
 
 @app.route('/')
@@ -21,11 +23,26 @@ def index():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for("index"))
+
     form = LoginForm()
 
     if form.validate_on_submit():
-        flash("Os dados foram enviados com sucesso. Seu usuário é {} e sua senha é {}"
-            .format(form.username.data, form.password.data))
-        redirect(url_for("index"))
+        user = User.query.filter(username=form.username.data).first()
+
+        if user is None or user.check_password(form.password.data):
+            flash("O nome de usuário ou senha está inválido.")
+            return redirect(url_for("login"))
+
+        login_user(user, remember_me=form.remember_me.data)
+
+        return redirect(url_for("index"))
 
     return render_template('login.html', title='Entrar', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
