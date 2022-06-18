c65182b5a1cacf6c6f541b6c8eb96f2da5ce7af4
Cristiano <cris.moraes.ds@gmail.com>
2022-06-18 16:11:48 -0300

    Adicionar página para visualizar perfil e editar perfil

--- app/forms.py
+++ app/forms.py
@@ -1,7 +1,7 @@
 from tkinter.messagebox import NO
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired, Email, EqualTo, ValidationError
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField
+from wtforms.validators import DataRequired, Email, EqualTo, ValidationError, Length
 
 from app.models import User
 
@@ -30,3 +30,8 @@ class RegistrationForm(FlaskForm):
 
         if user is not None:
             raise ValidationError("Um usuário com este e-mail já existe.")
+
+class EditProfileForm(FlaskForm):
+    username = StringField('Nome de usuário', validators=[DataRequired()])
+    about_me = TextAreaField('Sobre mim', validators=[Length(min=0, max=140)])
+    submit = SubmitField('Salvar perfil')
--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,17 @@
 from flask import render_template, flash, redirect, url_for, request
 from flask_login import current_user, login_user, logout_user, login_required
+from datetime import datetime
 
 from app import app, db
-from app.forms import LoginForm, RegistrationForm
+from app.forms import EditProfileForm, LoginForm, RegistrationForm
 from app.models import User
 from app.util import is_absolute_url
 
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.utcnow()
+        db.session.commit()
 
 @app.route('/')
 @login_required
@@ -77,3 +83,39 @@ def register():
         return redirect(url_for('login'))
     
     return render_template('register.html', title='Registrar-se', form=form)
+
+@app.route('/user/<username>')
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+
+    posts = [
+        {
+            'author': { 'username': 'criskell' },
+            'body': 'Aprendendo Flask!'
+        },
+        {
+            'author': { 'username': 'criskell' },
+            'body': 'Preciso fazer as lições de casa!'
+        }
+    ]
+
+    return render_template('user.html', title='Perfil', user=user, posts=posts)
+
+@app.route('/edit_profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.about_me = form.about_me.data
+        db.session.commit()
+        flash('As alterações foram salvas com sucesso.')
+        return redirect(url_for('edit_profile'))
+
+    if request.method == 'GET':
+        form.username.data = current_user.username
+        form.about_me.data = current_user.about_me
+    
+    return render_template('edit_profile.html', title='Editar perfil', form=form)
