e2faa875656d32ffc95b670362e6b19af6ace463
Cristiano <cris.moraes.ds@gmail.com>
2022-06-18 16:11:35 -0300

    Adicionar novos campos e avatar

--- app/models.py
+++ app/models.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
+from hashlib import md5
 
 from app import db, login
 
@@ -14,6 +15,8 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
     posts = db.relationship('Post', backref='author', lazy='dynamic')
+    about_me = db.Column(db.String(140))
+    last_seen = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
@@ -23,6 +26,10 @@ class User(db.Model, UserMixin):
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+    def avatar(self, size=64):
+        hash = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(hash, size)
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
