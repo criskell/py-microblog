330e0aab29b07387d7ec6546bcbf0c336477d8c6
Cristiano <cris.moraes.ds@gmail.com>
2022-06-25 16:33:11 -0300

    Adicionar log de erros
    - Via e-mail
    - Via arquivos

--- config.py
+++ config.py
@@ -7,3 +7,12 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY', 'COLOQUE-UMA-CHAVE-SUPERSECRETA-AQUI')
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI') or 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    MAIL_FROM_ADDR = os.environ.get('MAIL_FROM_ADDR')
+
+    ADMINS = (os.environ.get('ADMINS') or '').split(",")
