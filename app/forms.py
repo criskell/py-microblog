f6b79017b5587da141264f09fd8ccfdcb574aeb3
Cristiano <cris.moraes.ds@gmail.com>
2022-06-25 17:31:04 -0300

    Corrigir falha no nome de usuário
    Impedir que o usuário tente editar seu nome de usuário para um que já existe.

--- app/forms.py
+++ app/forms.py
@@ -1,4 +1,3 @@
-from tkinter.messagebox import NO
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField
 from wtforms.validators import DataRequired, Email, EqualTo, ValidationError, Length
@@ -35,3 +34,14 @@ class EditProfileForm(FlaskForm):
     username = StringField('Nome de usuário', validators=[DataRequired()])
     about_me = TextAreaField('Sobre mim', validators=[Length(min=0, max=140)])
     submit = SubmitField('Salvar perfil')
+
+    def __init__(self, original_username, *args, **kwargs):
+        super(EditProfileForm, self).__init__(*args, **kwargs)
+        self.original_username = original_username
+
+    def validate_username(self, username):
+        if username.data != self.original_username:
+            user = User.query.filter_by(username=self.username.data).first()
+
+            if user is not None:
+                raise ValidationError('Por favor digite um nome de usuário diferente.')
